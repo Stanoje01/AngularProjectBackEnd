f7575ae1b8c3a9514900ddce39e47986163cd42d
Stanoje01 <stanojevic.sasa03041@gmail.com>
2024-03-25 12:29:15 +0100

    Add CORS policy

--- AngularProjekatBackend/src/main/java/com/example/AngularProjekatBackend/Dao/KorisnikDao.java
+++ AngularProjekatBackend/src/main/java/com/example/AngularProjekatBackend/Dao/KorisnikDao.java
@@ -2,9 +2,11 @@ package com.example.AngularProjekatBackend.Dao;
 
 import com.example.AngularProjekatBackend.Entity.KorisnikEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableJpaRepositories
 public interface KorisnikDao extends JpaRepository<KorisnikEntity, Integer> {
     KorisnikEntity findByUsername(String username);
 
--- AngularProjekatBackend/src/main/java/com/example/AngularProjekatBackend/Service/KorisnikService.java
+++ AngularProjekatBackend/src/main/java/com/example/AngularProjekatBackend/Service/KorisnikService.java
@@ -11,9 +11,6 @@ public class KorisnikService {
     @Autowired
     private KorisnikDao korisnikDao;
 
-    @Autowired
-    // private PasswordEncoder passwordEncoder;
-
     public void registrujKorisnika(KorisnikEntity korisnik) {
         if (korisnikDao.findByUsername(korisnik.getUsername()) != null ||
                 korisnikDao.findByEmail(korisnik.getEmail()) != null) {
@@ -23,9 +20,9 @@ public class KorisnikService {
             korisnikDao.save(korisnik);
         }
     }
-
     public String login (String username, String password){
         KorisnikEntity korisnik = korisnikDao.findByUsername(username);
+
 
         if(korisnik == null || !korisnik.getPassword().equals(password)){
             throw new RuntimeException("Neipravno korisnicko ime ili lozinka!");
